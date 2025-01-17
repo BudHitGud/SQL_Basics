bd9c5da355e52cd18b939a675d4a05099792c3a1
XR_JAM <152742117+BudHitGud@users.noreply.github.com>
2025-01-17 14:34:52 +0100

    Primera carga
    uso de php, carga de imagen, propuesta inicial

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+
+
+
+</head>
+
+
+<body>
+
+
+
+<?php 
+
+print "<h1>Bienvenido a la pagina PHP </h1><br>";
+print "Vamos a trabajar las conexiones a bases de datos y consultas SQL! <br>";
+print "Aqui se muestra el esquema relacional de la base de datos propuesta<br>";
+?>
+
+<img src="imagenes/transacciones.jpg" alt="EsquemaER_Transacciones" style="width:1200px;height:600px;"></img>
+
+<?php 
+print "<h2>Queda por hacer: Paso a tablas, inscripcion de registros y hacer un CRUD basico</h2>";
+
+
+print "<p>Tambien voy a usar una variable ya por probar</p>";
+
+   $nombre = "Javier";
+
+print "Me llamo " . $nombre . " y aqui viene la variable, aunque se mantiene constante:<br>";
+print $nombre;
+
+?>
+
+
+
+
+</body>
+
+</html>
